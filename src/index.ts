55d3baed73ca3c1bfe1d4563b17e12efdded83c6
clevessonmendonca <wanderbloks311@gmail.com>
2023-02-03 15:53:57 -0300

    feat: add GetUsersController and GetUsersRepository

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello')
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+
+const app = express()
+
+const port = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+app.listen(port, () => console.log(`Running on port: ${port}`))
